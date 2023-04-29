8156384573e04434345de1516c1d3f1f1fd339ea
Cccc-owo <net4hhhh@163.com>
2023-04-29 11:22:06 +0800

    ci: update build script

--- main.bat
+++ main.bat
@@ -1,88 +1,50 @@
-::=====setting environment=====
+:check
 @ECHO OFF
+ECHO Time:%date%
+ECHO Checking...
+if not exist output\ ( mkdir output\ ) else ( del /F /S /Q output\ & rmdir /S /Q output\ & mkdir output\ )
+if exist *.zip del /F /S /Q *.zip
+goto select
+
+:select
+if exist output/font-GongWenKaiTi.mod ( ECHO. ) else ( goto set_GongWenKaiTi )
+if exist output/font-GuDianMingChaoTi.mod ( ECHO. ) else ( goto set_GuDianMingChaoTi )
+if exist output/font-KangXiZiDianTi.mod ( ECHO. ) else ( goto set_KangXiZiDianTi )
+if exist output/font-Source-Han-Serif.mod ( ECHO. ) else ( goto set_Source-Han-Serif )
+goto end
+
+:set_GongWenKaiTi
 set mod_desc=font-GongWenKaiTi.mod
 set mod_dir=font-GongWenKaiTi
-::=====setting environment end=====
-::=====checking=====
-ECHO Time:%date%
-ECHO Checking the required folder...
-if not exist output\ ( mkdir output\ ) else ( del /F /S /Q output\ & rmdir /S /Q output\ & mkdir output\ )
-if not exist output\%mod_dir% mkdir output\%mod_dir%\
-if exist *.zip del /F /S /Q *.zip
-::=====checking end=====
-::=====processing=====
-xcopy %mod_dir%\ output\%mod_dir% /E /Q /C /Y
-xcopy mod_descriptor\%mod_desc% output\ /E /Q /C /Y
-ECHO path="mod/%mod_dir%" >> output\%mod_desc%
-xcopy mod_descriptor\%mod_desc% output\%mod_dir%\ /Q /C /Y
-ren output\%mod_dir%\%mod_desc% descriptor.mod
-ECHO.
-ECHO Compressing the mod files...
-7z.exe a %mod_dir%.zip .\output\%mod_dir%*
-::=====processing end=====
+goto process
 
-::=====setting environment=====
-@ECHO OFF
+:set_GuDianMingChaoTi
 set mod_desc=font-GuDianMingChaoTi.mod
 set mod_dir=font-GuDianMingChaoTi
-::=====setting environment end=====
-::=====checking=====
-ECHO Time:%date%
-ECHO Checking the required folder...
-if not exist output\ ( mkdir output\ ) else ( del /F /S /Q output\ & rmdir /S /Q output\ & mkdir output\ )
-if not exist output\%mod_dir% mkdir output\%mod_dir%\
-::=====checking end=====
-::=====processing=====
-xcopy %mod_dir%\ output\%mod_dir% /E /Q /C /Y
-xcopy mod_descriptor\%mod_desc% output\ /E /Q /C /Y
-ECHO path="mod/%mod_dir%" >> output\%mod_desc%
-xcopy mod_descriptor\%mod_desc% output\%mod_dir%\ /Q /C /Y
-ren output\%mod_dir%\%mod_desc% descriptor.mod
-ECHO.
-ECHO Compressing the mod files...
-7z.exe a %mod_dir%.zip .\output\%mod_dir%*
-::=====processing end=====
+goto process
 
-::=====setting environment=====
-@ECHO OFF
+:set_KangXiZiDianTi
 set mod_desc=font-KangXiZiDianTi.mod
 set mod_dir=font-KangXiZiDianTi
-::=====setting environment end=====
-::=====checking=====
-ECHO Time:%date%
-ECHO Checking the required folder...
-if not exist output\%mod_dir% mkdir output\%mod_dir%\
-::=====checking end=====
-::=====processing=====
-xcopy %mod_dir%\ output\%mod_dir% /E /Q /C /Y
-xcopy mod_descriptor\%mod_desc% output\ /E /Q /C /Y
-ECHO path="mod/%mod_dir%" >> output\%mod_desc%
-xcopy mod_descriptor\%mod_desc% output\%mod_dir%\ /Q /C /Y
-ren output\%mod_dir%\%mod_desc% descriptor.mod
-ECHO.
-ECHO Compressing the mod files...
-7z.exe a %mod_dir%.zip .\output\%mod_dir%*
-::=====processing end=====
+goto process
 
-::=====setting environment=====
-@ECHO OFF
+:set_Source-Han-Serif
 set mod_desc=font-Source-Han-Serif.mod
 set mod_dir=font-Source-Han-Serif
-::=====setting environment end=====
-::=====checking=====
-ECHO Time:%date%
-ECHO Checking the required folder...
+goto process
+
+:process
 if not exist output\%mod_dir% mkdir output\%mod_dir%\
-::=====checking end=====
-::=====processing=====
 xcopy %mod_dir%\ output\%mod_dir% /E /Q /C /Y
 xcopy mod_descriptor\%mod_desc% output\ /E /Q /C /Y
-ECHO path="mod/%mod_dir%" >> output\%mod_desc%
+ECHO path="mod/%mod_dir%">> output\%mod_desc%
 xcopy mod_descriptor\%mod_desc% output\%mod_dir%\ /Q /C /Y
 ren output\%mod_dir%\%mod_desc% descriptor.mod
 ECHO.
 ECHO Compressing the mod files...
 7z.exe a %mod_dir%.zip .\output\%mod_dir%*
-::=====processing end=====
+goto select
+
+:end
 ECHO.
 ECHO End of script running.
